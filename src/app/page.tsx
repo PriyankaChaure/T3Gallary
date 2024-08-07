60e0bc7eb0e80ad96af4109faf95aebfd0e38d99
Priyanka <priyankachaure0393@gmail.com>
2024-08-07 17:13:39 +0530

    Scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/b614e88c-a08c-4a93-afab-b97d81e09b17-7h6zwl.jfif",
+  "https://utfs.io/f/933b6787-e5c6-4f10-bad8-07b505d51632-kaqg93.jfif",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     <h1>Welcome to Gallary!!!</h1>
+    <main className="">
+    
+      <div className="flex flex-wrap gap-5">
+        {[...mockImages,...mockImages,...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={`Image ${image.id}`} />
+          </div>
+        ))}
+      </div>
     </main>
   );
-}
+ }
