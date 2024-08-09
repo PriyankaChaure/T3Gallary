0d59dc176664057dc1e4e38ead64f08399404fe6
Priyanka <priyankachaure0393@gmail.com>
2024-08-09 13:42:54 +0530

    add loading spinner

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useUploadThing } from "../utils/uploadthing";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
@@ -33,11 +33,29 @@ function UploadSVG()
     );
 }
 
+function LoadingSpinnerSVG()
+{
+    return(
+    //     <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    //     <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" className="spinner_P7sC"/></svg>
+//     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+//     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+//   </svg> 
+<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z" className="spinner_5nOS"/></svg>
+
+);
+}
+
+//window.makeToast = makeUploadToast;
+
 export function SimpleUploadButton(){
+    
     const router = useRouter();
    const {inputProps} = useUploadThingInputProps("imageUploader",{
     onUploadBegin(){
-        toast("Uploading...",{
+        toast(
+        <div><LoadingSpinnerSVG/>Uploading...</div>,
+        {
             duration : 100000,
             id : "upload-begin",
         });
