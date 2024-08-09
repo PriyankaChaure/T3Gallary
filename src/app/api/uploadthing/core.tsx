89d716e3f9363ab5be5615d30e92e51033e73ad0
Priyanka <priyankachaure0393@gmail.com>
2024-08-09 17:20:31 +0530

    final

--- src/app/api/uploadthing/core.tsx
+++ src/app/api/uploadthing/core.tsx
@@ -3,27 +3,28 @@ import { UploadThingError } from "uploadthing/server";
 import { auth } from "@clerk/nextjs/server";
 import { db } from "~/server/db";
 import { imagess } from "~/server/db/schema";
+import { ratelimit } from "~/server/ratelimit";
 
  
 const f = createUploadthing();
-  
-// FileRouter for your app, can contain multiple FileRoutes
+
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
+ 
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount:40} })
-    // Set permissions and file types for this FileRoute
+
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
+    
       const user = auth();
  
-      // If you throw, the user will not be able to upload
+     
       if (!user.userId) throw new UploadThingError("Unauthorized");
- 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      const {success} = await ratelimit.limit(user.userId);
+      if (!success) throw new UploadThingError("RateLimited");
+   
       return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
+ 
       console.log("Upload complete for userId:", metadata.userId);
        
       await db.insert(imagess).values({
@@ -33,7 +34,7 @@ export const ourFileRouter = {
         })
      // console.log("file url", file.url);
  
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+   
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
